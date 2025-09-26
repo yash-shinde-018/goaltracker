7447e33640fc36540c3101a2c5dcf6f19ea1ab24
yash-shinde-018 <yashvikasshinde000@gmail.com>
2025-09-26 08:04:48 +0530

    Updated project with new features and fixes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // <--- Add this line for correct relative paths
   plugins: [react()],
   build: {
     outDir: 'dist',
