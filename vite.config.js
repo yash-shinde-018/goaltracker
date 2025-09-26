353afd5d787935f4e7892c0fe13ffe4e34c1dc5c
yash-shinde-018 <yashvikasshinde000@gmail.com>
2025-09-26 08:26:34 +0530

    Describe your update here

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // <--- Add this line for correct relative paths
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
